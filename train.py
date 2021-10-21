254b73de3bb6cd56db48836f76cba91d76f4eef4
eritsi <40695945+eritsi@users.noreply.github.com>
2021-10-21 10:25:57 +0900

    hello_world
    Neptune.aiのサンプル

---
+++ train.py
@@ -0,0 +1,16 @@
+import neptune.new as neptune
+
+run = neptune.init(
+    project="eritsi/staple",
+    api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI0MTc5MWU2Ny1mZWJkLTRlYWYtYmY3Mi02MWY4YzdhMzg5ZDMifQ==",
+)  # your credentials
+
+params = {"learning_rate": 0.001, "optimizer": "Adam"}
+run["parameters"] = params
+
+for epoch in range(10):
+    run["train/loss"].log(0.9 ** epoch)
+
+run["eval/f1_score"] = 0.66
+
+run.stop()
